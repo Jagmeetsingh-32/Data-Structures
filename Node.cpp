9cf604f9577e9c0bb77e2105324219129105e8ff
Jagmeetsingh-32 <jagmeetsingh3095@gmail.com>
2025-05-22 13:45:30 +0530

    first commit

---
+++ Node.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+class Node{
+    public:
+     int data;
+     Node*next;
+
+     Node(int a){
+        data=a;
+        next=NULL;
+     }
+};
+int main(){
+    Node*head;
+    head=new Node(15);
+    cout<<head->data<<endl;
+    cout<<head->next;
+    cout<<"hello..";
+}
